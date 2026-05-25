16c53bccee2a2e530d85fc7930e3ee74ff53e1fe
Abeer Garges <agfayez2018@gmail.com>
2026-05-25 09:16:44 -0700

    complete SandwichSize enum

--- src/main/java/com/pluralsight/model/Sandwich.java
+++ src/main/java/com/pluralsight/model/Sandwich.java
@@ -1,11 +1,32 @@
 package com.pluralsight.model;
 
-import javax.swing.*;
-
 public class Sandwich {
-    private int size;
+    private SandwichSize sandwichSize;
     private String breadType;
     private Topping topping;
     private boolean toasted;
+    private double price;
+}
+
+enum SandwichSize{
+    FOUR_INCH(4, 5.50),
+    EIGHT_INCH(8, 7.00),
+    TWELVE_INCH(12, 8.50);
+
+    private final int inches;
+    private final double basePrice;
+
+    SandwichSize(int inches, double basePrice) {
+        this.inches = inches;
+        this.basePrice = basePrice;
+    }
+
+    public int getInches() {
+        return inches;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
 }
 
